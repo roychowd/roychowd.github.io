b8b2af04b1f4dd035ddef743d8688dfaf35b3f67
Roy Chowdhury <konishky@ualberta.ca>
2020-01-26 17:19:26 -0700

    Updates

---
+++ precache-manifest.8983f959bd528725caeb37ed482eb550.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5277804c90fef2b8190b6569ccf2c7",
+    "url": "/perosnalwebsite/index.html"
+  },
+  {
+    "revision": "bebc706f834c9dbe1c64",
+    "url": "/perosnalwebsite/static/css/main.2f690c3d.chunk.css"
+  },
+  {
+    "revision": "d2cff589e382a7e5121c",
+    "url": "/perosnalwebsite/static/js/2.56b6f022.chunk.js"
+  },
+  {
+    "revision": "52ece5cef12d2ddcff30135306197eeb",
+    "url": "/perosnalwebsite/static/js/2.56b6f022.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bebc706f834c9dbe1c64",
+    "url": "/perosnalwebsite/static/js/main.fd5619f4.chunk.js"
+  },
+  {
+    "revision": "2d939e11e85f525c4ea4",
+    "url": "/perosnalwebsite/static/js/runtime-main.c2c2f5fd.js"
+  },
+  {
+    "revision": "9cdb7fca717eca574ceea232c09c0941",
+    "url": "/perosnalwebsite/static/media/resume.9cdb7fca.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6320bc0dbcd9ddae94843fb7ad6dab7d.js"
+  "/perosnalwebsite/precache-manifest.8983f959bd528725caeb37ed482eb550.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/perosnalwebsite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
